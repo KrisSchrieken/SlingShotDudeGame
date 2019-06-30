02d5b207dee0c530e6ee5b150861b86312da539b
KrisSchrieken <46463042+KrisSchrieken@users.noreply.github.com>
2019-06-29 20:16:15 -0700

    KeyBinds Setup

---
+++ SlingShotDudeGame/Assets/KeyBinds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBinds : MonoBehaviour
+{
+
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        keys.Add("Up", KeyCode.Space);
+        keys.Add("Down", KeyCode.s);
+        keys.Add("Right", KeyCode.d);
+        keys.Add("Left", KeyCode.a); 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public KeyCode getKey(String keyName) {
+        return keys[keyName];
+    }
+}
--- SlingShotDudeGame/Assets/PlayerMovement.cs
+++ SlingShotDudeGame/Assets/PlayerMovement.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    // movement speeds 
+    private const float UP_SPEED = 2.0F;
+    private const float DOWN_SPEED = -1.5F;
+    private const float RIGHT_SPEED = 5.0F;
+    private const float LEFT_SPEED = -5.0F; 
+
+    public Rigidbody dude; 
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log("Movement Started");
+      Debug.Log("Movement Started");
+        dude.useGravity() = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( Input.GetKey("w") ) {
+            dude.AddForce(0, UP_SPEED * Time.deltaTime, 0);    
+        }
+
+        if (Input.GetKey("s")) {
+            dude.AddForce(0, DOWN_SPEED * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("a")) {
+            dude.AddForce(0, LEFT_SPEED * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("d")) {
+            dude.AddFoce(0, RIGHT_SPEED * Time.deltaTime, 0);
+        }
+
     }
 }
