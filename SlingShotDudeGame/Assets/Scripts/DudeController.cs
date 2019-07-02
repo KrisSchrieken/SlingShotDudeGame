4b70c295cd32158e0cbc2df3cd325abe8e9cdaea
KrisSchrieken <46463042+KrisSchrieken@users.noreply.github.com>
2019-07-02 12:53:16 -0700

    Removed previous movement scripts for a physics a player controller script

---
+++ SlingShotDudeGame/Assets/Scripts/DudeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeController : Physics
+{
+
+    private const float JUMP_VELOCITY = 7f;
+    private const float MAX_VELOCITY = 7f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void ComputeUserVelocity() {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump")) {
+
+            velocity.y = JUMP_VELOCITY;
+        }
+
+        targetVelocity = move * MAX_VELOCITY; 
+    }
+}
---
+++ SlingShotDudeGame/Assets/Scripts/Physics.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour
+{
+    protected Rigidbody2D dude;
+
+    // gravity vars
+    protected Vector2 velocity;
+    public float gravityModifier = 1f;
+
+    protected Vector2 targetVelocity;
+
+    // to get user velocity from key inputs 
+    protected virtual void ComputeUserVelocity() { }
+
+    void OnEnable() {
+        dude = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetVelocity = Vector2.zero;
+        ComputeUserVelocity();
+    }
+
+
+    // for rigid body movements 
+    void FixedUpdate() {
+
+        // gravity velocity
+        velocity += Physics2D.gravity * Time.deltaTime * gravityModifier;
+
+        velocity.x = targetVelocity.x; // left/right movement 
+
+        Vector2 deltaPos = velocity * Time.deltaTime;
+        Vector2 move = Vector2.right * deltaPos.x;
+        Movement(move, false);
+
+        move = Vector2.up * deltaPos.y;
+
+        Movement(move, true); 
+    }
+
+    void Movement(Vector2 move, bool yMov) {
+        dude.position += move; 
+    }
+}
